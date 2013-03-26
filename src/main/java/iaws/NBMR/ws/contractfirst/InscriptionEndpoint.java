39735e12e12cb88d3fe0d2c9839843361d709007
Nathanaël Bertrand <cmoinath@gmail.com>
2013-03-26 19:28:45 +0100

    COnstruction reponse endpoint inscription

--- src/main/java/iaws/NBMR/ws/contractfirst/InscriptionEndpoint.java
+++ src/main/java/iaws/NBMR/ws/contractfirst/InscriptionEndpoint.java
@@ -28,21 +28,37 @@ public class InscriptionEndpoint {
 						 @XPathParam("/inscriptionRequest/inscription:email") String email,
 						 @XPathParam("/inscriptionRequest/inscription:adresse") String adresse){
 		
+		System.out.println("-----------> Enter the hanler");
+		
 		Utilisateur utilisateur = new Utilisateur(nom, prenom, email, adresse);
 		
-		try {
-			inscriptionService.inscrireUtilisateur(utilisateur);
-		} catch (CustomException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+
+		// On crée la réponse
 		Namespace namespace = Namespace.getNamespace("inscriptionResponse", "http://coUps/InscriptionSchema");
-		Element element = new Element("inscriptionResponse", namespace);
+		Element racine = new Element("inscriptionResponse", namespace);
         Element valeur = new Element("valeur", namespace);
-        element.addContent(valeur);
+        racine.addContent(valeur);
         
-		return element;
+		try {
+			// On tente d'inscrire notre utilisateur
+			inscriptionService.inscrireUtilisateur(utilisateur);
+			valeur.setText("OK");
+		} catch (CustomException e) {
+			valeur.setText("KO");
+			
+			// On crée le détail de l'erreur
+			Element erreur = new Element("erreur", namespace);
+			Element code = new Element("code", namespace);
+			Element message = new Element("message", namespace);
+			code.setText(""+e.getCode());
+			message.setText(e.getMessage());
+			erreur.addContent(code);
+			erreur.addContent(message);
+		}
+        
+		System.out.println("FIn handler");
+		
+		return racine;
 	}
 
 }
